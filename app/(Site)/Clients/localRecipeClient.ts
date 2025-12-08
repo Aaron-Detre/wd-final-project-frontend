865dad99ed8eda70cbf2cf4b227dcfa02b9437c5
Aaron Detre <aarondetre@gmail.com>
2025-12-07 19:39:51 -0800

    axios with credentials

--- app/(Site)/Clients/localRecipeClient.ts
+++ app/(Site)/Clients/localRecipeClient.ts
@@ -5,9 +5,10 @@ const axiosWithCredentials = axios.create({ withCredentials: true });
 const HTTP_SERVER = process.env.NEXT_PUBLIC_HTTP_SERVER;
 const RECIPES_API = `${HTTP_SERVER}/api/recipes`;
 
-//TODO: axios or axiosWithCredentials?
 export const filterRecipesByTitle = async (query: string) => {
-  const response = await axios.get(`${RECIPES_API}?title=${query}`);
+  const response = await axiosWithCredentials.get(
+    `${RECIPES_API}?title=${query}`
+  );
   return response.data;
 };
 export const getRecipeById = async (recipeId: string) => {
@@ -17,11 +18,13 @@ export const getRecipeById = async (recipeId: string) => {
   return response.data;
 };
 export const getAllLocalRecipes = async () => {
-  const response = await axios.get(`${RECIPES_API}`);
+  const response = await axiosWithCredentials.get(`${RECIPES_API}`);
   return response.data;
 };
 export const getFollowingRecipes = async (userId: string) => {
-  const response = await axios.get(`${RECIPES_API}/following/${userId}`);
+  const response = await axiosWithCredentials.get(
+    `${RECIPES_API}/following/${userId}`
+  );
   return response.data;
 };
 export const createRecipe = async (recipe: Recipe) => {
